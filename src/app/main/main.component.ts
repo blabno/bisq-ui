62c83dd858747966721f3d18f91b1302a8def6a1
Borowy Tomasz <borowy.tomasz@gmail.com>
2018-03-14 13:09:31 +0100

    Add OffersList component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,14 +1,11 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'app-main',
-  templateUrl: 'main.html'
+  templateUrl: 'main.component.html'
 })
 export class MainComponent {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor() {
   }
-
 }
