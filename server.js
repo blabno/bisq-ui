c67763450b684d515db39cf38243e7e5ed4e2469
Borowy Tomasz <borowy.tomasz@gmail.com>
2018-03-26 17:28:26 +0200

    Add docker compose to run bisq-ui

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const port = 8888;
+const app = express();
+
+const publicDir = `${__dirname}/www`;
+app.use(express.static(publicDir));
+app.get('*', (req, res) => res.sendFile(path.resolve(publicDir, 'index.html')));
+
+app.listen(port);
+console.info('Server started');
