7df70767d9a16da168639667a1d30c510110c12a
Dawid Zegar <davvvid1@gmail.com>
2018-03-27 17:21:45 +0200

    Use bootstrap nested navbar instead of ionic tabs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,22 @@
 import {Routes} from '@angular/router';
 import {MarketComponent} from './market/market.component';
-import {AccountComponent} from './account/account.component';
-import {SettingsComponent} from './settings/settings.component';
-import {OffersComponent} from "./offers/offers.component";
-import {PortfolioComponent} from './portfolio/portfolio.component';
+import {OffersComponent} from './offers/offers.component';
 import {CreateOffersComponent} from './offers/createOffers/createOffers.component';
-import {FundsComponent} from "./funds/funds.component";
 import {TakeOfferComponent} from './offers/takeOffer/takeOffer.component';
+import {ReceiveComponent} from './funds/receive/receive.component';
+import {SendComponent} from './funds/send/send.component';
+import {ReservedComponent} from './funds/reserved/reserved.component';
+import {LockedComponent} from './funds/locked/locked.component';
+import {TransactionComponent} from './funds/transactions/transaction.component';
+import {MyOpenOffersComponent} from './portfolio/myOpenOffers/myOpenOffers.component';
+import {OpenTradesComponent} from './portfolio/openTrades/openTrades.component';
+import {HistoryComponent} from './portfolio/history/history.component';
+import {PreferencesComponent} from './settings/preferences/preferences.component';
+import {NetworkInfoComponent} from './settings/network-info/network-info.component';
+import {AboutComponent} from './settings/about/about.component';
+import {CurrencyComponent} from './account/currency/currency.component';
+import {AltcoinsComponent} from './account/altcoins/altcoins.component';
+import {ArbitratorComponent} from './account/arbitrator/arbitrator.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: 'offers/buy', pathMatch: 'full'},
@@ -16,20 +26,27 @@ const appRoutes: Routes = [
   {path: 'offers/:type/take/:offerId', component: TakeOfferComponent},
   {path: 'offers/:type', component: OffersComponent},
 
-  {path: 'settings', redirectTo: 'settings/preferences', pathMatch: 'full'},
-  {path: 'settings/:type', component: SettingsComponent},
-
   {path: 'portfolio', redirectTo: 'portfolio/my-open-offers', pathMatch: 'full'},
-  {path: 'portfolio/:type', component: PortfolioComponent},
+  {path: 'portfolio/my-open-offers', component: MyOpenOffersComponent},
+  {path: 'portfolio/open-trades', component: OpenTradesComponent},
+  {path: 'portfolio/history', component: HistoryComponent},
 
   {path: 'funds', redirectTo: 'funds/receive', pathMatch: 'full'},
-  {path: 'funds/:type', component: FundsComponent},
+  {path: 'funds/receive', component: ReceiveComponent},
+  {path: 'funds/send', component: SendComponent},
+  {path: 'funds/reserved', component: ReservedComponent},
+  {path: 'funds/locked', component: LockedComponent},
+  {path: 'funds/transaction', component: TransactionComponent},
 
   {path: 'account', redirectTo: 'account/currency', pathMatch: 'full'},
-  {path: 'account/:type', component: AccountComponent},
+  {path: 'account/currency', component: CurrencyComponent},
+  {path: 'account/altcoins', component: AltcoinsComponent},
+  {path: 'account/arbitrator', component: ArbitratorComponent},
 
   {path: 'settings', redirectTo: 'settings/preferences', pathMatch: 'full'},
-  {path: 'settings/:type', component: SettingsComponent},
+  {path: 'settings/preferences', component: PreferencesComponent},
+  {path: 'settings/network-info', component: NetworkInfoComponent},
+  {path: 'settings/about', component: AboutComponent},
 
   {path: 'network', component: MarketComponent},
   {path: 'about', component: MarketComponent},
--- src/app/portfolio/openTrades/tradeList/tradeList.component.ts
+++ src/app/portfolio/openTrades/tradeList/tradeList.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, EventEmitter, Output, Input} from '@angular/core';
+import _ from 'lodash';
 
 import {TradesCacheService} from '../../../shared/services/tradesCache.service';
 
